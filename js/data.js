467507ecf65f90e5ec52c0d73f8ec7480a320102
quthangbuis <90095299+quthangbuis@users.noreply.github.com>
2021-11-19 17:02:20 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,227 @@
+const   NguyenTo = [
+            {
+                name: "Hydrogen",
+                symbol: "H",
+                transcribe: "/ˈhaɪdrədʒən/",
+                sound: "/audio/NguyenTo/Hydrogen.mp3"
+            },
+            {
+                name: "Helium",
+                symbol: "He",
+                transcribe: "/ˈhiːliəm/",
+                sound: "/audio/NguyenTo/Helium.mp3"
+            },
+            {
+                name: "Lithium",
+                symbol: "Li",
+                transcribe: "ˈ/lɪθiəm/",
+                sound: "/audio/NguyenTo/Lithium.mp3"
+            },
+            {
+                name: "Beryllium",
+                symbol: "Be",
+                transcribe: "/bəˈrɪliəm/",
+                sound: "/audio/NguyenTo/Beryllium.mp3"
+            },
+            {
+                name: "Boron",
+                symbol: "B",
+                transcribe: "/ˈbɔːrɒn/ <br> /ˈbɔːrɑːn/",
+                sound: "/audio/NguyenTo/Boron.mp3"
+            },
+            {
+                name: "Carbon",
+                symbol: "C",
+                transcribe: "/ˈkɑːbən/ <br> /ˈkɑːrbən/",
+                sound: "/audio/NguyenTo/Carbon.mp3"
+            },
+            {
+                name: "Nitrogen",
+                symbol: "N",
+                transcribe: "/ˈnaɪtrədʒən/",
+                sound: "/audio/NguyenTo/Nitrogen.mp3"
+            },
+            {
+                name: "Oxygen",
+                symbol: "O",
+                transcribe: "/ˈɒksɪdʒən/ <br> /ˈɑːksɪdʒən/",
+                sound: "/audio/NguyenTo/Oxygen.mp3"
+            },
+            {
+                name: "Fluorine",
+                symbol: "F",
+                transcribe: "/ˈflɔːriːn/ <br> /ˈflʊəriːn/ <br> /ˈflɔːriːn/ <br> /ˈflʊriːn/",
+                sound: "/audio/NguyenTo/Fluorine.mp3"
+            },
+            {
+                name: "Neon",
+                symbol: "Ne",
+                transcribe: "/ˈniːɒn/ <br> /ˈniːɑːn/",
+                sound: "/audio/NguyenTo/Neon.mp3"
+            },
+            {
+                name: "Sodium",
+                symbol: "Na",
+                transcribe: "/ˈsəʊdiəm/",
+                sound: "/audio/NguyenTo/Sodium.mp3"
+            },
+            {
+                name: "Magnesium",
+                symbol: "Mg",
+                transcribe: "/mæɡˈniːziəm/",
+                sound: "/audio/NguyenTo/Magnesium.mp3"
+            },
+            {
+                name: "Aluminium",
+                symbol: "Al",
+                transcribe: "/ˌæljəˈmɪniəm/ <br> /ˌæləˈmɪniəm/",
+                sound: "/audio/NguyenTo/Aluminium.mp3"
+            },
+            {
+                name: "Silicon",
+                symbol: "Si",
+                transcribe: "/ˈsɪlɪkən/",
+                sound: "/audio/NguyenTo/Silicon.mp3"
+            },
+            {
+                name: "Phosphorus",
+                symbol: "P",
+                transcribe: "/ˈfɒsfərəs/ <br> /ˈfɑːsfərəs/",
+                sound: "/audio/NguyenTo/Phosphorus.mp3"
+            },
+            {
+                name: "Sulfur",
+                symbol: "S",
+                transcribe: "/ˈsʌlfə(r)/ <br> /ˈsʌlfər/",
+                sound: "/audio/NguyenTo/Sulfur.mp3"
+            },
+            {
+                name: "Chlorine",
+                symbol: "Cl",
+                transcribe: "/ˈklɔːriːn/",
+                sound: "/audio/NguyenTo/Chlorine.mp3"
+            },
+            {
+                name: "Argon",
+                symbol: "Ar",
+                transcribe: "/ˈɑːɡɒn/ <br> /ˈɑːrɡɑːn/",
+                sound: "/audio/NguyenTo/Argon.mp3"
+            },
+            {
+                name: "Potassium",
+                symbol: "K",
+                transcribe: "/pəˈtæsiəm/",
+                sound: "/audio/NguyenTo/Potassium.mp3"
+            },
+            {
+                name: "Calcium",
+                symbol: "Ca",
+                transcribe: "/ˈkælsiəm/",
+                sound: "/audio/NguyenTo/Calcium.mp3"
+            },
+            {
+                name: "Scandium",
+                symbol: "Sc",
+                transcribe: "/ˈskændiəm/",
+                sound: "/audio/NguyenTo/Scandium.mp3"
+            },
+            {
+                name: "Titanium",
+                symbol: "Ti",
+                transcribe: "/tɪˈteɪniəm/ <br> /taɪˈteɪniəm/",
+                sound: "/audio/NguyenTo/Titanium.mp3"
+            },
+            {
+                name: "Vanadium",
+                symbol: "V",
+                transcribe: "/vəˈneɪdiəm/",
+                sound: "/audio/NguyenTo/Vanadium.mp3"
+            },
+            {
+                name: "Chromium",
+                symbol: "Cr",
+                transcribe: "/ˈkrəʊmiəm/",
+                sound: "/audio/NguyenTo/Chromium.mp3"
+            },
+            {
+                name: "Manganese",
+                symbol: "Mn",
+                transcribe: "/ˈmæŋɡəniːz/",
+                sound: "/audio/NguyenTo/Manganese.mp3"
+            },
+            {
+                name: "Iron",
+                symbol: "Fe",
+                transcribe: "/ˈaɪən/ <br> /ˈaɪərn/",
+                sound: "/audio/NguyenTo/Iron.mp3"
+            },
+            {
+                name: "Cobalt",
+                symbol: "Co",
+                transcribe: "/ˈkəʊbɔːlt/",
+                sound: "/audio/NguyenTo/Cobalt.mp3"
+            },
+            {
+                name: "Nickel",
+                symbol: "Ni",
+                transcribe: "/ˈnɪkl/",
+                sound: "/audio/NguyenTo/Nickel.mp3"
+            }
+            
+]
+
+const   Axit = [
+            {
+                name: "Hydrogen",
+                symbol: "H",
+                transcribe: "/ˈhaɪdrədʒən/",
+                sound: "/audio/NguyenTo/Hydrogen.mp3"
+            }
+        ]
+
+const   Bazo = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        transcribe: "/ˈhaɪdrədʒən/",
+        sound: "/audio/NguyenTo/Hydrogen.mp3"
+    }
+]
+
+const   Oxit = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        transcribe: "/ˈhaɪdrədʒən/",
+        sound: "/audio/NguyenTo/Hydrogen.mp3"
+    }
+]
+
+const   Muoi = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        transcribe: "/ˈhaɪdrədʒən/",
+        sound: "/audio/NguyenTo/Hydrogen.mp3"
+    }
+]
+
+const   Goc = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        transcribe: "/ˈhaɪdrədʒən/",
+        sound: "/audio/NguyenTo/Hydrogen.mp3"
+    }
+]
+
+const   ThuatNgu = [
+    {
+        name: "Hydrogen",
+        symbol: "H",
+        transcribe: "/ˈhaɪdrədʒən/",
+        sound: "/audio/NguyenTo/Hydrogen.mp3"
+    }
+]
+
+const   Items = [...NguyenTo,...Axit,...Bazo,...Oxit,...Muoi,...Goc,...ThuatNgu]
